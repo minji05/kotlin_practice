d43fd89c40bdf1876e2c44dc08b7d407e537dc26
minji05 <smj95133@likelion.org>
2023-09-19 02:09:51 +0900

    바인딩 구현 완료

--- viewbinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
+++ viewbinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.myButton.setOnClickListener {
+            binding.myTextView.text = "바인딩 완료!"
+        }
     }
 }
