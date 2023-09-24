7192d17ba937a03e0b95c5cd8edb3ab5c3dfc0a8
minji05 <smj95133@likelion.org>
2023-09-25 07:19:19 +0900

    [FEAT] : ⚙️ 레이아웃에 리사이클러뷰 위젯 정의하기+어댑터 연결하기

--- Recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ Recyclerview/app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -2,12 +2,45 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        //test
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 데이터 원본 준비
+        val dataList = mutableListOf<MyItem>()
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Bella", "1"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Charlie", "2"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Daisy", "1.5"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Duke", "1"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Max", "2"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Happy", "4"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Luna", "3"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Bob", "2"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Bob", "2"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Bob", "2"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Bob", "2"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Bob", "2"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Bob", "2"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Bob", "2"))
+        dataList.add(MyItem(R.drawable.ic_launcher_background, "Bob", "2"))
+
+
+       // binding.recyclerView.adapter = MyAdapter(dataList)
+
+        val adapter = MyAdapter(dataList)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+
     }
 }
