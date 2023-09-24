4b32457a0d3e9aab96d3142231409f1afee67090
minji05 <smj95133@likelion.org>
2023-09-25 07:50:02 +0900

    [FEAT] : ⚙️ MainActivity.kt에 Fragment 추가하기

--- fragment/app/src/main/java/com/example/fragment/MainActivity.kt
+++ fragment/app/src/main/java/com/example/fragment/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils.replace
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.example.fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.apply {
+            fragment1Btn.setOnClickListener{
+                setFragment(FirstFragment())
+            }
+            fragment2Btn.setOnClickListener {
+                setFragment(SecondFragment())
+            }
+        }
+        setFragment(FirstFragment())
+    }
+
+
+    private fun setFragment(frag: Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.frameLayout, frag)
+            setReorderingAllowed(true)
+            addToBackStack(null) // 빈 문자열 대신 null을 사용할 수 있습니다.
+        }
     }
 }
